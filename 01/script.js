b1097c76376a517b1771ea205fad4eaa16621ed8
Nazeer Ahmad <nazeerahmad2022@outlook.com>
2024-10-25 21:21:02 +0000

    script

--- 01/script.js
+++ 01/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+// value for the const number is not allowed to start it from 0 it always start from 1
+const accountId = 1262834682
+let accountNo = "03030130907"
+// if you want to use variables then give pereference to const and let. on the other hand var is not preferable
+var accountEmail = "ahmad29415@gmail.com";
+accountCity = "Islamabad"
+console.log(accountId);
+console.table([accountId, accountEmail, accountNo, accountCity ])
+
