ac4f2219389cf93ac6d34fb9f2ed1c3a7c89bb44
Nazeer Ahmad <nazeerahmad2022@outlook.com>
2024-10-25 21:20:50 +0000

    dt

---
+++ 01/dt.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code according to new version 
+// alert("hello") we are using node.js not browser
+
+let name1 = "nazeer"
+let age  =  "23"
+let isLoggedIn = true
+
+console.table([name1, age, isLoggedIn]);
+console.log(name1);
+console.log(age);
+console.log(isLoggedIn);
+
+
+/*DataTypes 
+1- BigInt  if the number is to large
+2- number 2 power to 53 max
+3- string 
+4- float
+5- boolean 
+6- null
+7- Undefined    
+8- Symbol
+*/
+let a = "Hi"
+console.log(typeof "a");
